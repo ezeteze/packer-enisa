c3a1aa30013a918e021812e84e49f5d1df21d16a
Chris Sidiropoulos <csidiropoulos@gmail.com>
2015-08-06 11:37:49 +0300

    Packages script: Install required packages.
    Fetch from apt required packages and install.

---
+++ script/packages.sh
@@ -0,0 +1,9 @@
+apt-get -y install wireshark tshark
+apt-get -y install volatility volatility-* libdistorm64-dev yara
+apt-get -y install lxterminal xarchiver leafpad software-properties-common python-software-properties vim xrdp
+apt-get -y install subversion git pcregrep libpcre++-dev python-dev geoip-bin ghex wget
+apt-get -y install default-jdk default-jre ruby
+apt-get -y install thunderbird irssi
+apt-get -y install chromium-browser pepperflashplugin-nonfree
+apt-get -y install apache2 php5 php5-mcrypt
+php5enmod mcrypt
