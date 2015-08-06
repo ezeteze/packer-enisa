dcb8352845dfcd0aa53b870cab89fd30b8d00094
Chris Sidiropoulos <csidiropoulos@gmail.com>
2015-08-06 11:51:54 +0300

    Setup script: Script to be run on first boot to configure workspace.

---
+++ files/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# init
+	function pause(){
+   	  read -p "$*"
+	}
+ 
+echo "Hello, welcome to initial setup of the ENISA training material."
+pause 'Press [Enter] key to continue...'
+unzip /home/enisa/training.zip -d /home/enisa/Desktop/Training-Material
+mv /home/enisa/Desktop/Training-Material/workspace /home/enisa/workspace
+mv /home/enisa/Desktop/Training-Material/.thunderbird /home/enisa/.thunderbird
+mv /home/enisa/Desktop/Training-Material/.irssi /home/enisa/.irssi
+sudo dpkg -i /home/enisa/Desktop/Training-Material/MaltegoCarbonCE.v3.5.3.deb
+rm -rf /home/enisa/Desktop/Training-Material/MaltegoCarbonCE.v3.5.3.deb
+sudo mv /home/enisa/Desktop/Training-Material/html/* /var/www/html/
+rm -rf /home/enisa/Desktop/Training-Material/html/
+sudo chown -R www-data:www-data /var/www/
+echo "export PATH=$PATH:/home/enisa/android/adt-bundle-linux-x86-20140702/sdk/platform-tools:/home/enisa/android/adt-bundle-linux-x86-20140702/sdk/tools" >> /home/enisa/.bash_profile
+echo "export ANDROID_HOME=/home/enisa/android/adt-bundle-linux-x86-20140702/sdk/" >> /home/enisa/.bash_profile
+echo "export PATH=$PATH:/home/enisa/android/adt-bundle-linux-x86-20140702/sdk/platform-tools:/home/enisa/android/adt-bundle-linux-x86-20140702/sdk/tools" >> /home/enisa/.profile
+echo "export ANDROID_HOME=/home/enisa/android/adt-bundle-linux-x86-20140702/sdk/" >> /home/enisa/.profile
+source /home/enisa/.bash_profile
+unzip /home/enisa/android.zip -d /home/enisa/
+chmod -R 777 /home/enisa/android/
+echo "no" | /home/enisa/android/adt-bundle-linux-x86-20140702/sdk/tools/android create avd -n Enisa -t 2 --abi default/armeabi-v7a
+echo "hw.keyboard=yes" >> /home/enisa/.android/avd/Enisa.avd/config.ini
+echo "hw.dPad=yes" >> /home/enisa/.android/avd/Enisa.avd/config.ini
+/home/enisa/android/adt-bundle-linux-x86-20140702/sdk//tools/mksdcard 100M /home/enisa/.android/avd/Enisa.avd/sdcard.img
+rm -rf /home/enisa/setup.sh
+rm -rf /home/enisa/training.zip
+rm -rf /home/enisa/android.zip
+pause 'Will now reboot! Press [Enter] key to continue...'
+sudo reboot
+
