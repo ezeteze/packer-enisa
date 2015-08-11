98483c1587e811237757cba8582895d0ae893646
Chris Sidiropoulos <csidiropoulos@gmail.com>
2015-08-11 11:43:55 +0300

    Install volatility from source.

--- script/packages.sh
+++ script/packages.sh
@@ -1,5 +1,5 @@
 apt-get -y install wireshark tshark
-apt-get -y install volatility volatility-* libdistorm64-dev yara
+apt-get -y install libdistorm64-dev yara
 apt-get -y install lxterminal xarchiver leafpad software-properties-common python-software-properties vim xrdp
 apt-get -y install subversion git pcregrep libpcre++-dev python-dev geoip-bin ghex wget
 apt-get -y install default-jdk default-jre ruby
---
+++ script/volatility.sh
@@ -0,0 +1,4 @@
+wget -O /tmp/volatility.zip https://github.com/volatilityfoundation/volatility/archive/master.zip
+unzip /tmp/volatility.zip -d /tmp
+cd /tmp/volatility-master
+python setup.py install
